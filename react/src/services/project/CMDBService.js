666c01fcfead383e19513daf7918ce7565bdbd3a
guoyinlong <guoyl@itnova.com.cn>
2021-01-05 15:39:50 +0800

    删除代码

--- react/src/services/project/CMDBService.js
+++
@@ -1,8 +0,0 @@
-import request, {getRequest, getRequestHasParam}  from '../../utils/request';
-
-export function queryCMDBData(param) {
-    return request('/microservice/newproject/cmdb/getResourceDetail', param);
-  }
-export function initedCMDBData(){
-   return getRequest('/microservice/newproject/cmdb/authToken');
-}
--- react/test/css-null-compiler.js
+++
@@ -1,3 +0,0 @@
-function noop() {  return null; }
-require.extensions['.less'] = noop;
-// you can add whatever you wanna handle require.extensions['.scss'] = noop; require.extensions['.css'] = noop; // ..etc
